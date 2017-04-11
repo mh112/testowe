8e39c88328b81c1298f51adb183247b9e5c2206c
Marek Hypki <57930@student.ue.poznan.pl>
2017-04-11 19:36:06 +0200

    cos tam

--- kalkulator.py
+++ kalkulator.py
@@ -2,5 +2,9 @@ def dodawanie():
   a = int(input("Podaj pierwsz¹ liczbê"))
   b = int(input("Podaj drug¹ liczbê"))
   print(a + b)
-  
+
+def get_help():
+	print("To jest prosty program kalkulatora. WprowadŸ dwie liczby i zatwierdŸ.")
+
+get_help()
 dodawanie()
